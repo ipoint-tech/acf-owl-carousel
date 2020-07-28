7a479a3b1be0ee4d79e974a3b38fed1c8a40ba3d
degger <degger@ipoint-tech.com>
2020-07-28 13:11:33 -0600

    add owl carousel scripts

--- acf-owl-carousel.php
+++ acf-owl-carousel.php
@@ -19,9 +19,16 @@ function acfoc_add_styles() {
 
 add_action( 'wp_enqueue_scripts', 'acfoc_add_styles' );
 
+
+
 function acfoc_add_scripts() {
+  wp_register_script('oc_scripts', plugins_url('owlcarousel/owl.carousel.min.js', __FILE__), array('jquery'),'1.1', true);
+
+  wp_enqueue_script('oc_scripts');
 
 }
+
+add_action( 'wp_enqueue_scripts', 'acfoc_add_scripts' );
 
 // create shortcode for displaying Carousel
 
