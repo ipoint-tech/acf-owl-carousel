1da01f5f56d3414233c0b52ca9b0eac8a3f3fc51
degger <degger@ipoint-tech.com>
2020-07-28 11:33:26 -0600

    Update acf-owl-carousel.php

--- acf-owl-carousel.php
+++ acf-owl-carousel.php
@@ -1,10 +1,8 @@
 /**
  * Plugin Name: ACF Owl Carousel
- * Plugin URI: http://www.mywebsite.com/my-first-plugin
+ * Plugin URI: https://github.com/ipoint-tech/acf-owl-carousel
  * Description: Owl Carousel Slider populated with Advanced Custom Fields
  * Version: 1.0
  * Author: Degger
  * Author URI: https://ipoint-tech.com
  */
- 
- 
