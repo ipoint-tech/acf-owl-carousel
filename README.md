6dfb5fb78dedc50d337198961f8734a518ca69a0
degger <degger@ipoint-tech.com>
2020-07-28 11:16:34 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# acf-owl-carousel
+ Wordpress Plugin to create an Owl Carousel Slider populated with Advanced Custom Fields
---
+++ acf-owl-carousel.php
@@ -0,0 +1,10 @@
+/**
+ * Plugin Name: ACF Owl Carousel
+ * Plugin URI: http://www.mywebsite.com/my-first-plugin
+ * Description: Owl Carousel Slider populated with Advanced Custom Fields
+ * Version: 1.0
+ * Author: Degger
+ * Author URI: https://ipoint-tech.com
+ */
+ 
+ 
