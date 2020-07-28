4d95bd2c41fbc1c073221df1fa8d80ee939a3e43
degger <degger@ipoint-tech.com>
2020-07-28 13:01:05 -0600

    add owl carousel files and enque style

--- acf-owl-carousel.php
+++ acf-owl-carousel.php
@@ -8,16 +8,29 @@
  * Author URI: https://ipoint-tech.com
  */
 
+// first things first, let's add the scripts and styles we need.
+function acfoc_add_styles() {
+  wp_register_style('oc_styles', plugins_url('lib/owlcarousel/owl.carousel.min.css', __FILE__));
+
+  wp_enqueue_style('oc_styles');
+}
+
+add_action( 'wp_enqueue_scripts', 'acfoc_add_styles' );
+
+function acfoc_add_scripts() {
+
+}
+
 // create shortcode for displaying Carousel
 
-function acf_custom_slider() {
+function acfoc_custom_slider() {
   $html = "<div class='owl-carousel'><p>hello</p></div>";
 
   return $html;
 
 }
 
-add_shortcode('custom-slider', 'acf_custom_slider');
+add_shortcode('custom-slider', 'acfoc_custom_slider');
 
 
 /* testing */
